5f51eb971bc6f321fd54e6cb8aae10b0cff02778
yosef salmalian <flaslnm@gmail.com>
2020-11-23 20:26:43 +0330

    slide tuto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    {{msg}}
+    <div v-if="show_msg">{{msg}}</div>
+
+    <div v-if="show_msg">true</div>
+    <div v-else-if="!sho_msg">false</div>
+    <div v-else>none</div>
+
+    <div v-for="(item, index) in msg_list" :key="index">
+      <hr>
+      <h3><span>{{index + 1}} </span>{{item}}</h3>
+    </div>
+
+    <button @click="toggle_show_msg($event)">toggle me</button>
   </div>
 </template>
 
@@ -12,6 +23,11 @@ export default {
       msg: 'Hello World!',
       show_msg: true,
       msg_list: ['hi', 'hola', 'hallow',  'salama'],
+    }
+  }, 
+  methods: {
+    toggle_show_msg() {
+      this.show_msg = !this.show_msg
     }
   }
 }
@@ -25,5 +41,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+span {
+  font-size: 16px;
 }
 </style>
