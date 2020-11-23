090b5513feb6e654d5a41b909b2f021872103053
yosef salmalian <flaslnm@gmail.com>
2020-11-23 19:50:04 +0330

    post comp

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,21 @@ export default {
   name: 'App',
   data() {
     return {
-      msg: 'Hello World!'
+      msg: 'Hello World!',
+      show_msg: true,
+      msg_list: ['hi', 'hola', 'hallow',  'salama'],
     }
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
