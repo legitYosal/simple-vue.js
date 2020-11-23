bb139d5369e6678ec6259aacfa7e488d0ca83a8e
yosef salmalian <flaslnm@gmail.com>
2020-11-23 20:44:30 +0330

    post component

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,37 @@
     </div>
 
     <button @click="toggle_show_msg($event)">toggle me</button>
+
+    <hr>
+    <hr>
+    <post-comp 
+      @change_text="change_post_text"
+      :text="post_text" />
   </div>
 </template>
 
 <script>
+import Post from './components/Post.vue'
+
 export default {
   name: 'App',
+  components: {
+    'post-comp': Post,
+  },
   data() {
     return {
       msg: 'Hello World!',
       show_msg: true,
       msg_list: ['hi', 'hola', 'hallow',  'salama'],
+      post_text: 'this is a random string for post to show off',
     }
   }, 
   methods: {
     toggle_show_msg() {
       this.show_msg = !this.show_msg
+    },
+    change_post_text(new_text) {
+      this.post_text = new_text
     }
   }
 }
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,10 +1,53 @@
 <template>
+    <div class="post">
+        Post
+        <p v-if="!editing" class="post-text">{{text}}</p>
+        <input v-else v-model="edited_text" type="text">
+        <button @click="toggle_edit">
+            <div v-if="editing">submit</div>
+            <div v-else>edit</div>
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'Post',
+    props: {
+        text: {type: String, default: 'Enter text'},
+    },
+    data() {
+        return {
+            editing: false,
+            edited_text: null,
+        }
+    },
+    methods: {
+        toggle_edit() {
+            if (this.editing && this.text !== this.edited_text) {
+                // emit event to parent component
+                this.$emit('change_text', this.edited_text)
+            }
+            this.editing = !this.editing
+        }
+    },
+    // mounted is called after a component renders for one time
+    mounted() {
+        this.edited_text = this.text
+    }
 }
 </script>
 
 <style>
+.post {
+    background: rgb(210, 245, 141);
+    width: 30%;
+    margin: auto;
+    height: 100px;
+}
+.post-text {
+    background: #eee;
+    width: 90%;
+    margin: auto;
+}
 </style>
